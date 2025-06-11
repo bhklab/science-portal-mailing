bef88a9f21f41231d73752cf69a75b69b7425e2f
mattbocc <matthew.boccalon@live.ca>
2025-06-11 19:24:33 -0400

    Fixed typing error for sets being used when lists are expected

--- scraping_core/scrape_body.py
+++ scraping_core/scrape_body.py
@@ -15,7 +15,7 @@ def scrape_body(body_text: str):
         gse_link = f"https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE{gse_id}"
         links.add(gse_link)
 
-    matches = NCT_MENTION_PATTERN.findall(body_text)
+    matches = NCT_MENTION_PATTERN.findall(str(body_text))
     for nct_id in matches:
         nct_link = f"https://clinicaltrials.gov/ct2/show/NCT{nct_id}"
         links.add(nct_link)
