fa1ba98d10193be4628ae69e9af3060f57cf1647
bhklabZLupien <bhklab.zeaslupien@gmail.com>
2025-08-01 10:12:55 -0400

    improved commenting of clean_pub_name

--- general_scripting/clean_pub_name.py
+++ general_scripting/clean_pub_name.py
@@ -1,6 +1,6 @@
 """
 Description: This script connects to a MongoDB database and cleans up publication names by removing <xyz> tags (modified clean_pdf_links for it to work with names)
-Date: 2025-07-31
+Date: 2025-08-01
 Author: Zéas Lupien (bhklab.zeaslupien@gmail.com, zaslup@gmail.com)
 """
 #import all necessary libraries
