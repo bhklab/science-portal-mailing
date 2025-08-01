5fd22650a365af4492532510548d55324a53702e
bhklabZLupien <bhklab.zeaslupien@gmail.com>
2025-08-01 10:08:47 -0400

    improved commenting of clean_pub_name which removes <xyz...> tags from publication names

--- general_scripting/clean_pub_name.py
+++ general_scripting/clean_pub_name.py
@@ -1,9 +1,9 @@
 """
 Description: This script connects to a MongoDB database and cleans up publication names by removing <xyz> tags (modified clean_pdf_links for it to work with names)
-Date: 2023-10-31
+Date: 2025-07-31
 Author: Zéas Lupien (bhklab.zeaslupien@gmail.com, zaslup@gmail.com)
 """
-
+#import all necessary libraries
 import pymongo
 import os
 from dotenv import load_dotenv
@@ -17,19 +17,20 @@ client = pymongo.MongoClient(os.getenv("SP_DATABASE_STRING"))
 db = client[os.getenv("DATABASE")]
 pub_collection = db[os.getenv("PUBLICATION_COLLECTION")]
 
-# Find all publications with <.*?> tags in the name field 
-#including <scp>, </scp>, <sup>, </sup>, <sub>, </sub>
+# Find all publications with <.*?> tags in the name field including <scp>, </scp>, <sup>, </sup>, <sub>, </sub>
 query = {
     "name": {"$regex": "<.*?>"}
 }
 pubs = pub_collection.find(query)
 
+#count of names changed before cleaning
 changed_count = 0
 
+# Iterate through each publication and clean the name field
 for pub in pubs:
     original_name = pub.get("name", "")
     if original_name:
-        # Remove all tags like <.*?> (xyz...), <scp>, </scp>, <sup>, </sup>, <sub>, </sub>
+        # Remove all tags like <.*?> (xyz...), <scp>, </scp>, <sup>, </sup>, <sub>, </sub> re.sub allows for more complex patterns such as non specific patterns
         cleaned_name = re.sub(r"<.*?>", "", original_name)
         if cleaned_name != original_name:
             pub_collection.update_one(
@@ -38,4 +39,5 @@ for pub in pubs:
             )
             changed_count += 1
 
+# Print out the total number of names changed
 print(f"\nTotal names changed: {changed_count}")
