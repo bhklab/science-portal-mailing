86728d43333a02d236482dc42d375b66e390f507
bhklabZLupien <bhklab.zeaslupien@gmail.com>
2025-08-18 10:03:15 -0400

    modified the name clean portion of scrape.py and of clean_pub_name.py to also incorporate cleaning of &lt;.*?&gt; which had been detected in 11 publication names

--- routes/scrape.py
+++ routes/scrape.py
@@ -153,7 +153,7 @@ async def crossref_scrape(pub: Publication) -> Publication:
 
                 pub.PMID = pub.PMID if (pub.PMID and (pub.PMID != "" or pub.PMID != -1)) else -1
                 pub.date = data['message']['created']['date-time'][:10]   
-                pub.name = re.sub(r"\s{2,}", " ", re.sub(r"<.*?>", "", data['message']['title'][0].replace('&amp;', '&')))           
+                pub.name = re.sub(r"\s{2,}", " ", re.sub(r"<.*?>", "", re.sub(r"&lt;.*?&gt;","", data['message']['title'][0].replace('&amp;', '&'))))          
                 pub.journal = data['message'].get('container-title')[0].replace('&amp;', '&').replace("<i>", "").replace("</i>", "") if data['message'].get('container-title') else data['message'].get('institution')[0]['name'].replace('&amp;', '&').replace("<i>", "").replace("</i>", "")
                 pub.type = data['message'].get('type')
                 pub.authors = author_string if author_string != "" else pub.authors
