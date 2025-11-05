8b74344eabb78c9f3877ee7817343bed487ffea8
mattbocc <matthew.boccalon@live.ca>
2025-11-05 15:33:20 -0500

    bug: ensuring the platform correctly identifies subfolders/files correctly

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 from fastapi import FastAPI
 from routes.emails import router as emails_router
 from routes.scrape import router as scrape_router
@@ -5,6 +9,7 @@ from routes.scrape import router as scrape_router
 app = FastAPI()
 app.include_router(emails_router)
 app.include_router(scrape_router)
+
 
 @app.get("/")
 async def root():
